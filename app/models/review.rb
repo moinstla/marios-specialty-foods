fa808669bdd345af4e46179af1d4c7f65370c4a5
Monique St. Laurent <moinstla@gmail.com>
2017-06-30 16:32:02 -0700

    validates rating between 1 and 5

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,5 +2,5 @@ class Review < ActiveRecord::Base
   belongs_to :product
   validates :author, :presence => true
   validates :content, {:presence => true, length: { in: 5..25 }}
-  validates :rating, :presence => true
+  validates :rating, {:presence => true, inclusion: { in: 1..5, message: "rating must be between 1 and 5 "}}
 end
