544fc1ffa5482b55baaa12582610f04a278e0182
Monique St. Laurent <moinstla@gmail.com>
2017-06-30 18:19:09 -0700

    pass all integration specs

--- spec/features/new_reviews_pages_spec.rb
+++ spec/features/new_reviews_pages_spec.rb
@@ -3,12 +3,13 @@ require 'rails_helper'
 describe 'visit root route to product path, add review' do
   it 'navigates from root to product path' do
     product = Product.create(:name => 'Peaches', :cost => 5, :country =>'USA')
-    visit products_path(product)
+    visit products_path
+    first(:link, 'Peaches').click
     click_link 'Add a review'
     fill_in 'Author', :with => "Frank"
     fill_in 'Content', :with => "Delicious"
     fill_in 'Rating', :with => 5
     click_on 'Create Review'
-    expect(page).to have_content review.content
+    expect(page).to have_content 'Delicious'
   end
 end
